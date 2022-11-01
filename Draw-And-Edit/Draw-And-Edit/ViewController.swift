253930643def5521f5bf54e891e03086b4ad128c
Ivan <ivan.skif41@gmail.com>
2022-11-01 16:43:11 +0100

    Initial Commit

---
+++ Draw-And-Edit/Draw-And-Edit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Draw-And-Edit
+//
+//  Created by Ivan Pryhara on 1.11.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
