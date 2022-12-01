10a6c19d90f628afa77b76ec650698800ba0b9c2
Ivan <ivan.skif41@gmail.com>
2022-12-01 22:25:15 +0100

    Rearrange files.

---
+++ Draw-And-Edit/Draw-And-Edit/TextEditing/TextEditingManagers/IPNSLayoutManager.swift
@@ -0,0 +1,15 @@
+//
+//  IPNSLayoutManager.swift
+//  Draw-And-Edit
+//
+//  Created by Ivan Pryhara on 1.12.22.
+//
+
+import UIKit
+import Foundation
+import CoreGraphics
+
+
+class IPNSLayoutManager: NSLayoutManager {
+    
+}
