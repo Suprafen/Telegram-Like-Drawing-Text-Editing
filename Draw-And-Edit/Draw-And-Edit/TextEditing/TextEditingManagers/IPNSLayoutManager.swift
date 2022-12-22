894a4a914dc7a28a80dd4c50ca04b3d8fc79bb62
Ivan <ivan.skif41@gmail.com>
2022-12-22 09:26:13 +0100

    Add methods to calculate necessary points for building background stroke

--- Draw-And-Edit/Draw-And-Edit/TextEditing/TextEditingManagers/IPNSLayoutManager.swift
+++ Draw-And-Edit/Draw-And-Edit/TextEditing/TextEditingManagers/IPNSLayoutManager.swift
@@ -11,6 +11,9 @@ import CoreGraphics
 
 
 class IPNSLayoutManager: NSLayoutManager {
+    // 0-indexed dic that holds corresponding points udner these points keys which are hold under line's index
+    // The structure is next [0 : [ltc: (x:0,y:0), rtc: (x:20,y:0), ...], ...]
+    var strokePoints: [Int: [String: CGPoint]] = [:]
     
     override init() {
         super.init()
@@ -40,8 +43,12 @@ class IPNSLayoutManager: NSLayoutManager {
         let context = UIGraphicsGetCurrentContext()
         context?.saveGState()
         context?.translateBy(x: origin.x, y: origin.y)
+        // Since we errase all data before putting new, we don't need to calculate a number of lines
+        strokePoints = [:]
         
-        self.enumerateLineFragments(forGlyphRange: glyphRange) { rect, usedRect, textContainer, glyphRange, stop in
+        self.enumerateLineFragments(forGlyphRange: glyphRange) { [weak self] rect, usedRect, textContainer, glyphRange, stop in
+                    
+            self?.getPoints(from: usedRect)
             context?.addPath(UIBezierPath(roundedRect: usedRect, cornerRadius: 7).cgPath)
             
             context?.setFillColor(restoredAlphaColor.cgColor)
@@ -52,5 +59,24 @@ class IPNSLayoutManager: NSLayoutManager {
             
         }
         context?.restoreGState()
+    }
+
+    func getPoints(from rect: CGRect) {
+
+            let ltc: CGPoint = rect.origin
+            let rtc: CGPoint = CGPoint(x: rect.maxX, y: rect.minY)
+            let rbc: CGPoint = CGPoint(x: rect.maxX, y: rect.maxY)
+            let lbc: CGPoint = CGPoint(x: rect.minX, y: rect.maxY)
+        
+            appendPointsToStrokePoints(ltc: ltc, rtc: rtc, rbc: rbc, lbc: lbc)
+
+    }
+
+    func appendPointsToStrokePoints(ltc: CGPoint, rtc: CGPoint, rbc: CGPoint, lbc: CGPoint) {
+        let currentIndexOfStrokePoints: Int = strokePoints.count
+        
+        strokePoints[currentIndexOfStrokePoints] = ["ltc": ltc, "rtc": rtc, "rbc": rbc, "lbc": lbc]
+        
+        print(strokePoints)
     }
 }
