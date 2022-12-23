52a95cfb1229b9d8ee3e6f29aaa0aec36dbe4fd6
Ivan <ivan.skif41@gmail.com>
2022-12-23 01:38:26 +0100

    Make text view background to become one closed geometry form.

--- Draw-And-Edit/Draw-And-Edit/TextEditing/TextEditingManagers/IPNSLayoutManager.swift
+++ Draw-And-Edit/Draw-And-Edit/TextEditing/TextEditingManagers/IPNSLayoutManager.swift
@@ -15,6 +15,10 @@ class IPNSLayoutManager: NSLayoutManager {
     // The structure is next [0 : [ltc: (x:0,y:0), rtc: (x:20,y:0), ...], ...]
     var strokePoints: [Int: [String: CGPoint]] = [:]
     
+    var strokePath: UIBezierPath = UIBezierPath()
+    
+    var cornerRadius: CGFloat = 7
+    
     override init() {
         super.init()
     }
@@ -45,12 +49,14 @@ class IPNSLayoutManager: NSLayoutManager {
         context?.translateBy(x: origin.x, y: origin.y)
         // Since we errase all data before putting new, we don't need to calculate a number of lines
         strokePoints = [:]
+        // This won't shown the last line. Smth strange happen btw.
+        strokePath = UIBezierPath()
         
         self.enumerateLineFragments(forGlyphRange: glyphRange) { [weak self] rect, usedRect, textContainer, glyphRange, stop in
                     
             self?.getPoints(from: usedRect)
-            context?.addPath(UIBezierPath(roundedRect: usedRect, cornerRadius: 7).cgPath)
-            
+//            context?.addPath(UIBezierPath(roundedRect: usedRect, cornerRadius: 7).cgPath)
+            context?.addPath(self?.strokePath.cgPath ?? UIBezierPath().cgPath)
             context?.setFillColor(restoredAlphaColor.cgColor)
             context?.setStrokeColor(restoredAlphaColor.cgColor)
             context?.fillPath(using: .evenOdd)
@@ -69,6 +75,8 @@ class IPNSLayoutManager: NSLayoutManager {
             let lbc: CGPoint = CGPoint(x: rect.minX, y: rect.maxY)
         
             appendPointsToStrokePoints(ltc: ltc, rtc: rtc, rbc: rbc, lbc: lbc)
+        
+            buildPath()
 
     }
 
@@ -78,5 +86,56 @@ class IPNSLayoutManager: NSLayoutManager {
         strokePoints[currentIndexOfStrokePoints] = ["ltc": ltc, "rtc": rtc, "rbc": rbc, "lbc": lbc]
         
         print(strokePoints)
+    }
+    
+    func buildPath() {
+        let numberOfLines = strokePoints.count
+        
+        for i: Int in 0..<numberOfLines {
+            let currentLine: [String : CGPoint]? = strokePoints[i]
+            
+            guard let ltc: CGPoint = currentLine?["ltc"],
+                  let rtc: CGPoint = currentLine?["rtc"],
+                  let rbc: CGPoint = currentLine?["rbc"],
+                  let lbc: CGPoint = currentLine?["lbc"] else { return }
+            
+            if numberOfLines == 1 {
+                strokePath.move(to: ltc)
+                strokePath.addLine(to: rtc)
+                strokePath.addLine(to: rbc)
+                strokePath.addLine(to: lbc)
+                strokePath.addLine(to: ltc)
+            }
+            else {
+                if i == 0 {
+                    strokePath.move(to: ltc)
+                    strokePath.addLine(to: rtc)
+                    strokePath.addLine(to: rbc)
+                    // the last rectangle
+                } else if i == numberOfLines - 1 {
+                    strokePath.addLine(to: rtc)
+                    strokePath.addLine(to: rbc)
+                    
+                    var c = i
+                    
+                    while c > 0 {
+                        let previousLine: [String : CGPoint]? = strokePoints[c]
+                        
+                        guard let ltc: CGPoint = previousLine?["ltc"],
+                              let lbc: CGPoint = previousLine?["lbc"] else { return }
+                        
+                        strokePath.addLine(to: lbc)
+                        strokePath.addLine(to: ltc)
+                        
+                        c -= 1
+                    }
+                } else {
+                    strokePath.addLine(to: rtc)
+                    strokePath.addLine(to: rbc)
+                }
+            }
+        }
+        
+        strokePath.close()
     }
 }
